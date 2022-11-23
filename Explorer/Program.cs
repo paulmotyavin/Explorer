d9093cf6de0caec955e3013d8f655cb2bc949ae4
paulmotyavin <isip_p.d.motyavin@mpt.ru>
2022-11-23 22:37:07 +0300

    я точно доделаю/сделаю 6 и 7 практическую до следующей недели

---
+++ Explorer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Explorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Этот компьютер");
+            Console.WriteLine("--------------------------------------------");
+            //диск с
+            // диск d
+        }
+    }
+}
